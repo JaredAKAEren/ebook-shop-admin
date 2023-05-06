ecd8324cbd42cfb1c0cb744c03247575bf28b72b
Jared <shixuran@qq.com>
2023-05-06 11:23:29 +0800

    update-vite分开打包css文件的设置很麻烦，所以将分开引入tailwindcss解决冲突的方式改为通过layer覆盖base样式解决

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// import './assets/styles/preflight.css'
-// import './assets/styles/tailwind.css'
+import './assets/styles/tailwind.css'
 import 'vfonts/Lato.css'
 import {
     create,
