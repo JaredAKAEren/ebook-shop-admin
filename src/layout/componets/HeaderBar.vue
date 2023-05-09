04ee202b5d4186cd582e36243e7778278436433d
Jared <shixuran@qq.com>
2023-05-09 13:35:07 +0800

    build-新打包

--- src/layout/componets/HeaderBar.vue
+++ src/layout/componets/HeaderBar.vue
@@ -1,6 +1,7 @@
 <template>
-    <header class="h-12 flex items-center">头部，放一些用户数据</header>
-    <button type="button" @click="toLogout">登出</button>
+    <header class="h-12 flex items-center">头部，放一些用户数据
+        <button type="button" @click="toLogout">登出</button>
+    </header>
 </template>
 
 <script setup lang="ts">
